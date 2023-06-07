a1b5938b89b2d52559126271b1c647900952d556
samuellangajr <samuellangajr@gmail.com>
2023-06-08 00:15:25 +0200

    footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Footer />
       </header>
     </div>
   );
---
+++ front-end/src/components/footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div class="container">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Áureo Guite</a></li
+        ><li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Egan Peter</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Hassan Mutole</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Ivaldo Mavie</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Samuel Langa</a></li>
+      </ul>
+      <p class="text-center text-body-secondary">&copy; 2023 Gestão de eleições, Inc</p>
+    </footer>
+  </div>
+    );
+};
+
+export default Footer;
